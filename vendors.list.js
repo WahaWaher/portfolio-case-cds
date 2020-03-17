86b8616e4b9ac35c15010a18de0c8d3db882c864
WahaWaher <wahawaher@gmail.com>
2020-03-17 21:28:35 +0300

    comment vendor svgForEverybody

--- vendors.list.js
+++ vendors.list.js
@@ -5,7 +5,7 @@
 module.exports = {
   'jquery': './node_modules/jquery/dist/jquery.js',
   'jquery.animate.css': './node_modules/@wahawaher/jquery-animate-css/jquery.animate.css.js',
-  'svg4everybody': './node_modules/svg4everybody/dist/svg4everybody.js',
+  // 'svg4everybody': './node_modules/svg4everybody/dist/svg4everybody.js',
   'lazysizes.bundle': [
     './node_modules/lazysizes/plugins/unveilhooks/ls.unveilhooks.js',
     // './node_modules/lazysizes/plugins/bgset/ls.bgset.js',
